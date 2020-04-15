6e723ee0b9f5730b0aa300a50d1103e11068ef03
max <manbatminer@gmail.com>
2020-04-15 18:11:42 +0200

    Nice little maths thingy I just did, that squares and adds 3 to every number from 1 to 1000000

---
+++ Examples/Max/Example 002.py
@@ -0,0 +1,16 @@
+import datetime
+start = datetime.datetime.now()
+
+print "x|y"
+print "==="
+
+xs = range(1, 1000001)
+for x in xs:
+    y = x * x + 3
+    table = "{}|{}".format(x, y)
+    print table
+
+time_taken = datetime.datetime.now() - start
+
+print("calculated results in:{}".format( time_taken))
+print ("fastest time: 4.13 Seconds")
