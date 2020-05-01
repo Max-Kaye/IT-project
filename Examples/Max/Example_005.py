f295b36624b03d7b49100594bf392ed3a601d371
Ant Kutschera <ant.kutschera@gmail.com>
2020-05-01 20:20:08 +0200

    adding example for max to work on

---
+++ Examples/Max/Example_005.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+# https://en.wikipedia.org/wiki/Fibonacci_number
+def calculateFibonacci(first_number=1, second_number=1, size_of_output=3):
+    output = []
+
+    # fix me!
+
+    return output
+
+
+class Test(unittest.TestCase):
+
+    def test(self):
+        answer = calculateFibonacci(2, 3, 5)
+        self.assertEqual(answer.__len__(), 5)
+        self.assertEqual(answer, [2, 3, 5, 8, 11])
+
+        answer = calculateFibonacci(1, 10, 4)
+        self.assertEqual(answer.__len__(), 4)
+        self.assertEqual(answer, [1, 10, 11, 21])
