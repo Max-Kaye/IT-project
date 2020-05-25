0e725a3cdd7c577804f3a54b2a31f1126c6d3355
PierreCam05 <pierre.campos@icloud.com>
2020-05-25 16:24:21 +0200

    added an expected line in code

--- Examples/Pierre/Example_005_fibonacci2.py
+++ Examples/Pierre/Example_005_fibonacci2.py
@@ -2,22 +2,25 @@ import unittest
 import sys
 from termcolor import colored, cprint
 
+
 # https://en.wikipedia.org/wiki/Fibonacci_number
 def calculatefibonacci(first_number, second_number, size_of_output):
     output = [first_number, second_number]
-    while output.__len__()!= size_of_output:
-        third_number = first_number+second_number
+    while output.__len__() != size_of_output:
+        third_number = first_number + second_number
         output.append(third_number)
-        first_number=second_number
-        second_number=third_number
-        cprint (str(third_number), "green")
+        first_number = second_number
+        second_number = third_number
+        cprint(str(third_number), "green")
     return output
-input1=eval(input("Enter your first number: "))
-input2=eval(input("Enter your second number: "))
-input3=eval(input("How many results would you like: "))
-if int(input3)<=2:
+
+
+input1 = eval(input("Enter your first number: "))
+input2 = eval(input("Enter your second number: "))
+input3 = eval(input("How many results would you like: "))
+if int(input3) <= 2:
     sys.exit("You need a minimum of 3 results")
-answer = calculatefibonacci(input1, input2,input3)
+answer = calculatefibonacci(input1, input2, input3)
 if answer.__len__() != input3:
     print("failed")
 else:
