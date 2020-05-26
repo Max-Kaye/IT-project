6e6eb0aeff3f790419d237b1580c9184428ea1de
PierreCam05 <pierre.campos@icloud.com>
2020-05-26 21:31:29 +0200

    made it into a bot

--- Examples/Pierre/Today's Date.py
+++ Examples/Pierre/Today's Date.py
@@ -1,4 +1,25 @@
 from datetime import datetime
-now = datetime.now()
+import os
+import discord
 
-print("%02d/%02d/%04d %02d:%02d:%02d" % (now.day, now.month, now.year, now.hour, now.minute, now.second))
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        print("connected to guild %s" % guild)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return  # ignore messages sent by the bot
+
+    now = datetime.now()
+
+    words = message.content.split(" ")
+    if words[0] == "!date":
+        result=("%02d/%02d/%04d %02d:%02d:%02d" % (now.day, now.month, now.year, now.hour, now.minute, now.second))
+        await message.channel.send(result)
+
+client.run(os.getenv('DISCORD_TOKEN'))
