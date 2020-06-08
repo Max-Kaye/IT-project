d8b7986767da48c20c657583e91abe45fc4c84a6
PierreCam05 <pierre.campos@icloud.com>
2020-06-08 21:58:10 +0200

    Started working on a weather forecast command. Started working on changeable prefixes. Finalized the .weather command by adding changing icons depending on the the weather as well as making it into an embed. Added permissions to commands. Added spamping command, working on making it so only Whitelisted IDs can run it. started working on the spam command (same thing as spamping but with text). made error functions.

--- Examples/Pierre/Opal-Bot.py
+++ Examples/Pierre/Opal-Bot.py
@@ -1,14 +1,64 @@
+from textwrap import indent
+
 import discord
+from discord import guild
 from discord.ext import commands, tasks
 import os
 import random
 from itertools import cycle
 from flask import ctx
 import requests, json
+import urbandict
+import functools
+import itertools
+import asyncio
+import math
+import youtube_dl
+from async_timeout import timeout
+import pickle
+from datetime import datetime
+import pyowm
+import json
 
 client = commands.Bot(command_prefix=".")
 
+"""
+def get_prefix(client, message):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+
+client = commands.Bot(command_prefix=get_prefix)
+
+
+@client.event
+async def on_guild_join(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = "."
+
+    with open("prefixes.json", "w") as f:
+        json.dump = (prefixes, f)
+
+
+@client.event
+async def on_guild_remove(guild):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open("prefixes.json", "w") as f:
+        json.dump = (prefixes, f)
+
+    print(f"Opal has joined {guild}")
+"""
+
 status = cycle(["DM @PierreCam#6969 to add me to your server!", "Listening to your commands!"])
+
 
 
 @client.event
@@ -25,7 +75,6 @@ async def on_ready():
 async def status_loop():
     await client.change_presence(activity=discord.Game(next(status)))
 
-
 @client.event
 async def on_member_remove(member):
     print(f"{member} has left or been removed from a Server")
@@ -36,13 +85,147 @@ async def ping(ctx):
     await ctx.send(f"The Ping is at: {round(client.latency * 1000)} ms")
 
 
-@client.command()
-async def weather(ctx, *, city):
+Icons = {
+    # thunderstorm
+    "thunderstorm with light rain": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm with rain": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm with heavy rain": "http://openweathermap.org/img/wn/11d@2x.png",
+    "light thunderstorm": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm": "http://openweathermap.org/img/wn/11d@2x.png",
+    "heavy thunderstorm": "http://openweathermap.org/img/wn/11d@2x.png",
+    "ragged thunderstorm": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm with light drizzle": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm with drizzle": "http://openweathermap.org/img/wn/11d@2x.png",
+    "thunderstorm with heavy drizzle": "http://openweathermap.org/img/wn/11d@2x.png",
+    # drizzle
+    "light intensity drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "heavy intensity drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "light intensity drizzle rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "drizzle rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "heavy intensity drizzle rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "shower rain and drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "heavy shower rain and drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    "shower drizzle": "http://openweathermap.org/img/wn/09d@2x.png",
+    # rain
+    "light rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "moderate rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "heavy intensity rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "very heavy rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "extreme rain": "http://openweathermap.org/img/wn/10d@2x.png",
+    "freezing rain": "http://openweathermap.org/img/wn/13d@2x.png",
+    "light intensity shower rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "shower rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "heavy intensity shower rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    "ragged shower rain": "http://openweathermap.org/img/wn/09d@2x.png",
+    # snow
+    "light snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Heavy snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Sleet": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Light shower sleet": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Shower sleet": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Light rain and snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Rain and snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Light shower snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Shower snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    "Heavy shower snow": "http://openweathermap.org/img/wn/13d@2x.png",
+    # atmosphere
+    "mist": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Smoke": "http://openweathermap.org/img/wn/50d@2x.png",
+    "sand/ dust whirls": "http://openweathermap.org/img/wn/50d@2x.png",
+    "Haze": "http://openweathermap.org/img/wn/50d@2x.png",
+    "fog": "http://openweathermap.org/img/wn/50d@2x.png",
+    "dust": "http://openweathermap.org/img/wn/50d@2x.png",
+    "sand": "http://openweathermap.org/img/wn/50d@2x.png",
+    "volcanic ash": "http://openweathermap.org/img/wn/50d@2x.png",
+    "squalls": "http://openweathermap.org/img/wn/50d@2x.png",
+    "tornado": "http://openweathermap.org/img/wn/50d@2x.png",
+    # clear
+    "clear sky": "http://openweathermap.org/img/wn/01d@2x.png",
+    # clouds
+    "overcast clouds": "http://openweathermap.org/img/wn/04d@2x.png",
+    "broken clouds": "http://openweathermap.org/img/wn/04d@2x.png",
+    "scattered clouds": "http://openweathermap.org/img/wn/03d@2x.png",
+    "few clouds": "http://openweathermap.org/img/wn/02d@2x.png"
+}
+
+'''
+@client.command(aliases=["weather today"])
+async def weather_today(ctx, *, Location):
     api_key = "WEATHER_API_KEY"
 
-    base_url = "http://api.openweathermap.org/data/2.5/weather?appid=310b950bcd05029af4e22e41ca2315ed&q"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?appid=784ead9b61bd1b14ffc70bc90e0fe4de&q"
 
-    city_name = city
+    city_name = Location
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+
+    response = requests.get(complete_url)
+
+    x = response.json()
+
+    if x["cod"] != "404":
+
+        y = x["main"]
+
+        sunrisetd = pyowm.owm.daily.sunrise
+        sunsettd = pyowm.owm.daily.sunset
+
+        morntemp = pyowm.owm.daily.temp.morn
+        daytemp = pyowm.owm.daily.temp.day
+        evetemp = pyowm.owm.daily.temp.eve
+        nighttemp = pyowm.owm.daily.temp.night
+
+        maxtemp = pyowm.owm.daily.temp.max
+        mintemp = pyowm.owm.daily.temp.min
+
+        current_humidity = y["humidity"]
+
+        z = x["weather"]
+
+        weather_description = z[0]["description"]
+
+        sentence = weather_description
+        words = sentence.split(" ")
+        result = ""
+        for word in words:
+            result = result + word.capitalize() + " "
+
+        embed = discord.Embed(
+            colour=discord.Colour.blue(),
+            title=f"Weather in {Location}",
+            description=f"{result}",
+        )
+        try:
+            icon = Icons[weather_description]
+            embed.set_thumbnail(url=icon)
+        except KeyError:
+            pass
+
+        embed.add_field(name="Sunrise", value=f"{sunrisetd}")
+        embed.add_field(name="Sunset", value=f"{sunsettd}")
+        embed.add_field(name="Morning Temperature", value=f"{morntemp}°C")
+        embed.add_field(name="Mid-Day Temperature", value=f"{daytemp}°C")
+        embed.add_field(name="Evening Temperature", value=f"{evetemp}°C")
+        embed.add_field(name="Night Temperature", value=f"{nighttemp}°C")
+        embed.add_field(name="Maximum Temperature", value=f"{maxtemp}°C")
+        embed.add_field(name="Minimum Temperature", value=f"{mintemp}°C")
+        embed.set_footer(text="This System is Powered by openweathermap.org")
+
+        await ctx.send(embed=embed)
+    else:
+        await ctx.send("Location Not Found")
+'''
+
+
+@client.command(aliases=["WEATHER","Weather"])
+async def weather(ctx, *, Location):
+    api_key = "WEATHER_API_KEY"
+
+    base_url = "http://api.openweathermap.org/data/2.5/weather?appid=784ead9b61bd1b14ffc70bc90e0fe4de&q"
+
+    city_name = Location
 
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
 
@@ -56,6 +239,7 @@ async def weather(ctx, *, city):
 
         current_temperature = y["temp"]
         temp_centigrade = current_temperature - 273.15
+        temp_farenheit = (temp_centigrade * 9 / 5) + 32
         current_pressure = y["pressure"]
 
         current_humidity = y["humidity"]
@@ -64,12 +248,37 @@ async def weather(ctx, *, city):
 
         weather_description = z[0]["description"]
 
-        await ctx.send(f"**Temperature:** {round(temp_centigrade)}°C")
-        await ctx.send(f" **Atmospheric Pressure:** {current_pressure} hPa")
-        await ctx.send(f" **Humidity:** {current_humidity}%")
-        await ctx.send(f" **Weather Description:** {weather_description}")
+        sentence = weather_description
+        words = sentence.split(" ")
+        result = ""
+        for word in words:
+            wethdesc = result + word.capitalize() + " "
+
+        cityname = city_name
+        words = cityname.split(" ")
+        result = ""
+        for word in words:
+            capcity = result + word.capitalize() + " "
+
+        embed = discord.Embed(
+            colour=discord.Colour.dark_magenta(),
+            title=f"Weather in {capcity}",
+            description=f"{wethdesc}",
+        )
+        try:
+            icon = Icons[weather_description]
+            embed.set_thumbnail(url=icon)
+        except KeyError:
+            pass
+
+        embed.add_field(name="Temperature", value=f"{round(temp_centigrade)}°C/{round(temp_farenheit)}°F")
+        embed.add_field(name="Humidity", value=f"{current_humidity}%")
+        embed.add_field(name="Atmospheric Pressure", value=f"{current_pressure}pHa")
+        embed.set_footer(text="This System is Powered by openweathermap.org")
+
+        await ctx.send(embed=embed)
     else:
-        await ctx.send("City Not Found")
+        await ctx.send("Location Not Found")
 
 
 @client.command(aliases=["8ball"])
@@ -97,16 +306,48 @@ async def _8ball(ctx, *, question):
 @client.command()
 @commands.has_permissions(manage_messages=True)
 async def purge(ctx, amount: int):
-    await ctx.channel.purge(limit=amount)
+    await ctx.channel.purge(limit=amount + 1)
+
+'''
+@client.command()
+@commands.has_permissions(administrator=True)
+async def setprefix(ctx, prefix):
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open("prefixes.json", "w") as f:
+        json.dump = (prefixes, f)
+'''
+
+async def add_cache(self, search, result, t=0, level=1):
+    try:
+        try:
+            if result['error']:
+                return
+        except KeyError:
+            pass
+        if t == 0:
+            self.image_cache[search] = [result, level]
+        elif t == 1:
+            self.search_cache[search] = [result, level]
+        elif t == 2:
+            self.youtube_cache[search] = [result, level]
+    except Exception as e:
+        print(e)
 
 
 @client.command()
+@commands.has_permissions(kick_members=True)
 async def kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
     await ctx.send(f"{member.mention} has been kicked from a server for {reason}")
 
 
 @client.command()
+@commands.has_permissions(ban_members=True)
+@commands.has_permissions(administrator=True)
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f"{member.mention} has been banned from a server for: {reason}")
@@ -118,6 +359,7 @@ async def test(ctx):
 
 
 @client.command()
+@commands.has_permissions(ban_members=True)
 async def unban(ctx, *, member):
     banned_users = await ctx.guild.bans()
     member_name, member_discriminator = member.split('#')
@@ -142,10 +384,31 @@ async def gay(ctx, *, member):
     ]
     await ctx.send(f"{random.choice(responses)}")
 
-@client.command()
+
+@client.command(aliases=["howgay"])
 async def gayrate(ctx, *, member):
-    responses = random.randint(1, 100)
+    responses = random.randint(0, 100)
     await ctx.send(f"{member} is {responses}% gay")
+
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def spamping(ctx, member: discord.Member, amount):
+    for i in range(int(amount)):
+        await ctx.send(f"{member.mention}")
+    AmountAsInt = int(amount)
+    AmountAsInt = AmountAsInt + 1
+    await ctx.channel.purge(limit=AmountAsInt)
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def spam(ctx, text, amount):
+    for i in range(int(amount)):
+        await ctx.send(f"{text}")
+    AmountAsInt = int(amount)
+    AmountAsInt = AmountAsInt + 1
+    await ctx.channel.purge(limit=AmountAsInt)
+
 
 # FIX LATER
 # @client.command()
@@ -154,48 +417,82 @@ async def gayrate(ctx, *, member):
 #    channel = author.voice_channel
 #    await client.join_voice_channel(channel)
 
+@gay.error
+async def gay_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please use the format: `.gay {Insert User}`")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
+
+
+@spamping.error
+async def spamping_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please use the format: `.spamping {Insert User}, {Insert Amount of Pings}`")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
+
+
+@gayrate.error
+async def gayrate_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please use the format: `.gayrate {Insert User}` or `.howgay {Insert User}`")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
+
 
 @weather.error
 async def weather_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please use the format: `.weather {Insert Location}`")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
 @_8ball.error
 async def _8ball_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Please use the format: `.8ball {insert question}`")
+        await ctx.send("Please use the format: `.8ball {Insert Question}`")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
 @purge.error
 async def purge_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please specify the amount of messages you want to be purged")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
 @kick.error
 async def kick_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please specify the user you want to kick")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send("Please specify the user you want to ban")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
-# FIX LATER
-# @unban.error()
-# async def unban_error(ctx, error):
-#    if isinstance(error, commands.MissingRequiredArgument):
-#       await ctx.send("Please specify the user you want to unban")
+@unban.error
+async def unban_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please specify the user you want to unban")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("You do not have permission to run that command.")
 
 
-# @client.event()
-# async def on_command_error(ctx, error):
-#   if isinstance(error, commands.CommandNotFound):
-#      await ctx.send("Command does not exist")
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        print("User tried unexisting Command")
 
 
 client.run(os.getenv('TOKEN'))
